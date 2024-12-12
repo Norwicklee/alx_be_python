edfc232641eacaf22b7a6ac6398c99073c0d2705
Norwicklee <nomalisa.n1@gmail.com>
2024-12-12 11:39:16 +0200

    calculate the simple interest on a given principal amount, rate of interest, and time.

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,5 +1,6 @@
-principal = 1000
-rate = 0.05
-time = 3
+P = 1000
+R = 0.05
+T = 3
+#I =P*R*T
 
-print(f"The simple interest is : {principal * rate * time}")
+print(f"The simple interest is : {P * R * T}")
