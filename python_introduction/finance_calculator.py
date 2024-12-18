f6b31e8a16480842e9aa51e0ea73e127be788bb3
Norwicklee <nomalisa.n1@gmail.com>
2024-12-18 12:43:40 +0200

    Control flow

--- python_introduction/finance_calculator.py
+++
@@ -1,9 +0,0 @@
-monthly_income = float(input("Enter your monthly income:"))
-monthly_expenses = float(input("Enter your total monthly expenses:"))
-
-monthly_savings = monthly_income - monthly_expenses
-
-Projected_Savings = ((monthly_savings * 12 * 1.05))
-
-print(f"Your monthly savings are {monthly_savings}.")
-print(f"Projected savings after one year, with interest, is {Projected_Savings}.")
--- python_introduction/future_age_calculator.py
+++
@@ -1,4 +0,0 @@
-current_age = int(input("How old are you?"))
-age = current_age + 27
-
-print(f"In 2050, you will be {age} years old.")
